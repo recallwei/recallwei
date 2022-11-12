058e4b647df083b0c6b3db13d6223c72d6ffd16f
Bruce Song <recall4056@gmail.com>
2022-11-12 10:46:40 +0800

    Update.

--- nuxt-app/components/Navbar.vue
+++ nuxt-app/components/Navbar.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts"></script>
 
-<template><div class="navbar"></div></template>
+<template>
+  <div class="navbar-wrapper">
+    <div class="navbar"></div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
-.navbar {
-  background-color: wheat;
-  height: 60px;
-  width: auto;
-  margin: 12px;
-  border-radius: 8px;
+.navbar-wrapper {
+  padding: 12px;
+  .navbar {
+    height: 60px;
+    width: auto;
+    background-color: wheat;
+    border: 1px solid gray;
+    border-radius: 8px;
+  }
 }
 </style>
