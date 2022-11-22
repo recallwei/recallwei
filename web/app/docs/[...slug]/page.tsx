5817d394f074d497ffd5a36737be6346901b0782
Bruce Song <recall4056@gmail.com>
2022-11-23 00:07:01 +0800

    Update.

--- web/app/docs/[...slug]/page.tsx
+++ web/app/docs/[...slug]/page.tsx
@@ -1,16 +1,18 @@
 import styles from "./page.module.scss";
+import { useMDXComponent } from "next-contentlayer/hooks";
 import { allDocs } from "@contentlayer";
 import type { Doc } from "@contentlayer";
 
-export async function generateStaticParams() {
-  return allDocs
-    .filter((doc: Doc) => !doc.draft)
-    .map((doc: Doc) => ({
-      slug: doc.slug,
-    }));
-}
+// export async function generateStaticParams() {
+//   return allDocs[0];
+// }
 
-export default async function ({ params }: { params: { slug: string } }) {
-  console.log(params);
-  return <div></div>;
+export default async function () {
+  const MDXContent = useMDXComponent(allDocs[0].body.code);
+  const MDXComponents = {};
+  return (
+    <div>
+      <MDXContent components={MDXComponents} />
+    </div>
+  );
 }
