c10287ad653affd82e528b16a8a5c6885e0b3172
Bruce Song <recall4056@gmail.com>
2022-11-23 09:01:00 +0800

    Update.

--- web/app/docs/[...slug]/page.tsx
+++ web/app/docs/[...slug]/page.tsx
@@ -2,17 +2,22 @@ import styles from "./page.module.scss";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { allDocs } from "@contentlayer";
 import type { Doc } from "@contentlayer";
+import { useAppConfig } from "@hooks";
 
-// export async function generateStaticParams() {
-//   return allDocs[0];
-// }
+export async function generateStaticParams() {
+  const doc = allDocs[0];
+  console.log(doc);
+  return { props: { doc } };
+}
 
-export default async function () {
-  const MDXContent = useMDXComponent(allDocs[0].body.code);
-  const MDXComponents = {};
+const MyButton: React.FC = () => <button>Click me</button>;
+
+const Doc: React.FC<{ doc: any }> = ({ doc }) => {
+  const MDXContent = useMDXComponent(doc.body.code);
+  const MDXComponents = { MyButton };
   return (
     <div>
       <MDXContent components={MDXComponents} />
     </div>
   );
-}
+};
