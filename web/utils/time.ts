08f3361e58fc97592bda47fd1121392760f905ff
Bruce Song <recall4056@gmail.com>
2022-11-24 23:33:32 +0800

    Add time function.

--- web/utils/time.ts
+++ web/utils/time.ts
@@ -1,3 +1,9 @@
 import moment from "moment";
 
-export const formatTime = () => {};
+export const formatTime = (time?: string): string | null => {
+  if (!time) {
+    return null;
+  }
+  const date = new Date(time);
+  return moment(date).format("YYYY-MM-DD");
+};
