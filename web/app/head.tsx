2a4eff40ff54330d2bf1f5072cddf08617f60fe4
Bruce Song <recall4056@gmail.com>
2022-12-01 23:11:04 +0800

    Update.

--- web/app/head.tsx
+++ web/app/head.tsx
@@ -2,12 +2,18 @@ const Head = (): JSX.Element => {
   return (
     <>
       <title>Bruce Song</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta charSet="UTF-8" />
+      <link rel="icon" href="/favicon.png" />
       <meta
         name="description"
         content="I am Bruce Song, this is my personal website."
       />
-      <link rel="icon" href="/favicon.png" />
+      <meta name="keywords" content="Bruce Song, Blog, Doc, Personal Website" />
+      <meta name="author" content="Bruce Song" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0"
+      />
     </>
   );
 };
