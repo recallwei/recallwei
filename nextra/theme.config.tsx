576d329eb015f06bf65ccceb37da117845c7783f
Bruce <recall4056@gmail.com>
2022-12-27 18:57:25 +0800

    Update.

---
+++ nextra/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { DocsThemeConfig } from "nextra-theme-docs"
+
+const config: DocsThemeConfig = {
+  logo: <span>Bruce Song</span>,
+  project: {
+    link: "https://github.com/recallwei/recallwei"
+  },
+  chat: {
+    link: "https://discord.com"
+  },
+  docsRepositoryBase: "https://github.com/recallwei/recallwei",
+  footer: {
+    text: "Bruce Song"
+  }
+}
+
+export default config
