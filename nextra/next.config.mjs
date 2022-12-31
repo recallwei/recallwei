9c2c6a34bc82fec702cd3950dcb0864834457c1d
Bruce Song <recall4056@gmail.com>
2022-12-31 12:58:31 +0800

    Update.

---
+++ nextra/next.config.mjs
@@ -0,0 +1,16 @@
+import nextra from "nextra"
+
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  staticImage: true,
+  latex: true,
+  flexsearch: {
+    codeblocks: false
+  },
+  defaultShowCopyCode: true
+})
+
+export default withNextra({
+  reactStrictMode: true
+})
