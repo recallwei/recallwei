1ec37a5324755e08cb979eac700381c7c97a52f9
Bruce <recall4056@gmail.com>
2023-03-19 17:25:26 +0800

    chore: remove old project

--- components/StarCount/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useSSG } from "nextra/ssg"
-
-const StarCount = (): JSX.Element => {
-  const { starCount } = useSSG() || {}
-  return <strong>{starCount}</strong>
-}
-
-export default StarCount
--- middleware.ts
+++
@@ -1,3 +0,0 @@
-import { withLocales } from "nextra/locales"
-
-export const middleware = withLocales((request) => {})
