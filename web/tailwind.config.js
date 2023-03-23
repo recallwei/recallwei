5f8c580043469a2945b3cbb8184e2088ab4e4c7a
Bruce <recall4056@gmail.com>
2023-03-23 08:20:46 +0800

    style: format

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './mdx-components.tsx'],
+  content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+      './mdx-components.tsx'
+    ],
   theme: {
     extend: {
       fontFamily: {
