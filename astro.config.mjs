713278e1a6e7280dc936adda0e5ec12b0c7df272
Bruce <recall4056@gmail.com>
2023-04-14 16:30:35 +0800

    feat: add plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,20 @@
 import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import remarkToc from 'remark-toc'
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
+import rehypePresetMinify from 'rehype-preset-minify'
 
-export default defineConfig({})
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      remarkPlugins: [rehypeHeadingIds,remarkToc, rehypeAccessibleEmojis],
+      rehypePlugins: rehypePresetMinify.plugins,
+      remarkRehype: { footnoteLabel: 'Footnotes' },
+      gfm: false
+    })
+  ]
+})
