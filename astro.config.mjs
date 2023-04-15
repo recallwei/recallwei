d484d6b8357ac67e51edbc8dad1dd54c91878eb6
Bruce <recall4056@gmail.com>
2023-04-15 20:14:34 +0800

    chore: add react and configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
-import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import remarkToc from 'remark-toc'
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
 import rehypePresetMinify from 'rehype-preset-minify'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bruceworld.top',
   integrations: [
+    react(),
     mdx({
       syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'dracula' },
-      remarkPlugins: [rehypeHeadingIds,remarkToc, rehypeAccessibleEmojis],
+      shikiConfig: {
+        theme: 'dracula'
+      },
+      remarkPlugins: [rehypeHeadingIds, remarkToc, rehypeAccessibleEmojis],
       rehypePlugins: rehypePresetMinify.plugins,
-      remarkRehype: { footnoteLabel: 'Footnotes' },
+      remarkRehype: {
+        footnoteLabel: 'Footnotes'
+      },
       gfm: false
     })
   ]
