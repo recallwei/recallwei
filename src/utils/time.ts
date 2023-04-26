355a1d3dcd47b4858183882845a7c97abc8351b8
Bruce <recall4056@gmail.com>
2023-04-26 20:51:39 +0800

    feat: add navigate

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -6,6 +6,17 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const FILE_DEV_DIR = '../content/post'
+const FILE_PROD_DIR = '../../../src/content/post'
+
+export const getLastUpdatedTime = (id: string) => {
+  const fileDev = resolve(__dirname, FILE_DEV_DIR, id)
+  const fileProd = resolve(__dirname, FILE_PROD_DIR, id)
+  const file = existsSync(fileDev) ? fileDev : fileProd
+  const child = sync('git', ['log', '-1', '--pretty="%ci"', file])
+  const output = child.stdout.toString()
+  return new Date(output).toLocaleString()
+}
 
 export const formatDate = (date: string | Date, type: 0 | 1 | 2 = 0) => {
   switch (type) {
@@ -16,13 +27,4 @@ export const formatDate = (date: string | Date, type: 0 | 1 | 2 = 0) => {
     case 2:
       return dayjs(date).format('YYYY/MM')
   }
-}
-
-export const getLastUpdatedTime = (id: string) => {
-  const fileDev = resolve(__dirname, '../content/post', id)
-  const fileProd = resolve(__dirname, '../../../src/content/post', id)
-  const file = existsSync(fileDev) ? fileDev : fileProd
-  const child = sync('git', ['log', '-1', '--pretty="%ci"', file])
-  const output = child.stdout.toString()
-  return new Date(output).toLocaleString()
 }
