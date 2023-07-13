715a1f95e8543ebddb170035f40f3539818cb580
Bruce <recall4056@gmail.com>
2023-07-13 16:57:53 +0800

    chore: update

--- src/base/tag.ts
+++ src/base/tag.ts
@@ -19,6 +19,7 @@ export const TagList = <const>[
   'Prisma',
   'React',
   'SCSS',
+  'Server',
   'SVG',
   'TailwindCSS',
   'TypeScript',
---
+++ src/content/post/zh/2023-07-13-deploy-nodejs-api-on-cloud-server.md
@@ -0,0 +1,5 @@
+---
+title: '在云服务器上部署 Node.js API'
+tags:
+  - 'Server'
+---
